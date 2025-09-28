ae2cf27b4f5200654cbe201865f951ea71e4d967
Anton Bilous <anton.bilous.job@gmail.com>
2025-09-28 19:54:54 +0300

    save

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 def copy_file(command: str) -> None:
     parts = command.split(" ")
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
     source_file_name = parts[1]
     destination_file_name = parts[2]
 
-    if len(parts) != 3 or parts[0] != "cp" or source_file_name == destination_file_name:
+    if source_file_name == destination_file_name:
         return
 
     try:
-        with open(source_file_name, "r") as file_in, open(destination_file_name, "w") as file_out:
+        with (
+            open(source_file_name, "r") as file_in,
+            open(destination_file_name, "w") as file_out,
+        ):
             for line in file_in:
                 file_out.write(line)
     except Exception:
