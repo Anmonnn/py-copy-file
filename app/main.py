38b35ce825bbe07152eeb722d361526a45ac11fa
Anton Bilous <anton.bilous.job@gmail.com>
2025-09-28 17:48:23 +0300

    save

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    comm = command.split(" ")
+
+    if len(comm) < 3 or comm[0] != "cp" or comm[1] == comm[2]:
+        return
+
+    try:
+        with open(comm[1], "r") as file_in, open(comm[2], "w") as file_out:
+            for line in file_in:
+                file_out.write(line)
+    except Exception:
+        return
